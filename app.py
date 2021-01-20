29ea1f6dd81a47bd63dbca9095c26cf737b89c71
Agnieszka Bień <agnieszka.bien@virtual.com>
2021-01-20 22:54:09 +0100

    dodanie slownika

--- app.py
+++ app.py
@@ -7,36 +7,48 @@ app = Flask(__name__)
 app.secret_key = 'asdfasdfasdfaskjdlhflwgejnlhus'
 
 @app.route("/start_gra", methods=['POST', "GET"])
+
+# global lista_slow
+# # lista_slow = wczytaj_slowa
+
+
+
 def start_gry():
 
    global gra
-   gra = ''
    gra = Game()
-   gra.start_gry()
+   gra.init_game()
+   # gra.start_gry()
    return redirect(
       url_for('gra'))
 
 
 @app.route("/gra", methods=['POST', "GET"])
 def gra():
-
-   karty_gracza = ['BA', 'ka', 'wa', 'se']
-   karty_gracza = gra.gracze[gra.turn].karty_gracza
-   form = WybierzKarte()
-   print(form.validate_on_submit())
-   if form.validate_on_submit():
-      return f"{form.karta.data}"
-   return render_template("gra.html", karty_gracza=karty_gracza, form=form)
+    global gra
+    print(gra)
+    print(gra.gracze)
+    karty_gracza = gra.gracze[gra.turn].karty_gracza
+    form = WybierzKarte()
+    print(form.validate_on_submit())
+    print(form.potwierdz.data,form.karta.data)
+    # choice_form = ChooseCard(gra.gracze[gra.turn].karty_gracza)
+    if form.validate_on_submit():
+        gra.start_gry(form.karta.data)
+        if gra.koniec_gry:
+            return render_template("koniec_gry.html", wygrany_gracz = gra.turn, gracze= ['Agnieszka', 'Pawel'])
+        return redirect(url_for("gra"))
+        # return redirect(url_for("koniec_gry"))
+    return render_template("gra.html", karty_gracza=karty_gracza, form=form)
 
 if __name__ == "__main__":
-   app.run(host="0.0.0.0", port=5000, debug=True)
+    app.debug =True
+    app.run(host="0.0.0.0", port=5000, debug=True)
 
 
 
 #brakuje nam:
 #TODO:
 # sprawdznie czy karta moze poleciec na srodek
-# gra z komputerem
-# koniec gry, startowanie od nowa
-# forma z lista wybieralna
-# forma z dzialajacym POST
+# gra z komputerem # losowa karta z reki, 2. wybor trybu gry
+#
--- game.py
+++ game.py
@@ -1,53 +1,62 @@
-from flask import Flask, render_template, request
 import random
+
 
 class Game:
     # jakie atrybuty przyjmuje klasa
     # i jakie metody sa mozliwe
-    karty = ['by', 'ko', 'pu', 'le', 'na', 'py', 'ma', 'gi', 'ki', 'sza', 'li', 'fa', 'my', 'że', 'cza', 'tka', 'mi', 'nie', 'ni', 'cha', 'wo', 'la', 'ka', 'lu', 'ga', 'mu', 'sy', 'pa', 'ty', 'wy', 'by', 'ry', 'da', 'to', 'ra', 'fu', 'dy', 'chy', 'ru', 'ku', 'wa', 'dy', 'ta', 'ny', 'po', 'ła', 'do', 'ba', 'no', 'sa']
-    karty_gracza = 6
-    turn = 1
-    wartownik = ''
-    gracze = []
 
-    # def __init__(self, karty):
-    #     self.karty = karty
+    def __init__(self):
+        self.karty = ['by', 'ko', 'pu', 'le', 'na', 'py', 'ma', 'gi', 'ki', 'sza', 'li', 'fa', 'my', 'że', 'cza',
+                      'tka', 'mi', 'nie', 'ni', 'cha', 'wo', 'la', 'ka', 'lu', 'ga', 'mu', 'sy', 'pa', 'ty', 'wy',
+                      'by', 'ry', 'da', 'to', 'ra', 'fu', 'dy', 'chy', 'ru', 'ku', 'wa', 'dy', 'ta', 'ny', 'po', 'ła',
+                      'do', 'ba', 'no', 'sa']
+        self.karty_gracza = 6
+        self.turn = 1
+        self.wartownik = ''
+        self.gracze = []
+        self.koniec_gry = None
 
     def __str__(self):
         return f" {self.karty}"
 
-    def start_gry(self, tryb_gry='cpu'):
+    def init_game(self, tryb_gry='cpu'):
         random.shuffle(self.karty)
         self.wartownik = self.karty[-1]
         self.karty.pop()
-        self.lista_graczy = self.rozdaj_karty() # cpu = Player(['le', 'pu', 'ko', 'by', 'ki', 'wa']) return [cpu, p1]
-        # while True:
-        #     self.ruch()
-        #     if self.koniec_gry():
-        #         print(f'koniec gry wygral gracz {self.turn}')
-        #         break
-        #     self.zmiana_gracza()
-        return f"wygral gracz: {self.turn}"
+        self.lista_graczy = self.rozdaj_karty()
 
-    def ruch(self):
+    def start_gry(self, sylaba, tryb_gry='cpu'):
+        self.ruch(sylaba)
+        if self.czy_zakonczyc_gre():
+            return True
+        self.zmiana_gracza()
+
+
+    def ruch(self, sylaba):
         if self.turn == 0:
-            #self.ruch_cpu() # losowe ruchy z tych kart co ma w rece
-            self.ruch_gracza()
+            # self.ruch_cpu() # losowe ruchy z tych kart co ma w rece
+            self.ruch_gracza(sylaba)
         elif self.turn == 1:
-            self.ruch_gracza()
+            self.ruch_gracza(sylaba)
 
     def sprawdz_poprawnosc_slowa(self, wybrana_karta):
-        if len(self.wartownik + wybrana_karta) == 4:
+        if len(self.wartownik + wybrana_karta) >= 4:
             return True
         else:
             return False
 
+    def sprawdzanie_slowa_sjp(self, wybrana_karta):
+        pass
+
+
+
     def ruch_gracza(self, sylaba=None):
         print(self.gracze)
         print(self.gracze[self.turn].karty_gracza)
-        sylaba = input('wybierz karte ktora chcesz zagrac')
+        # sylaba = input('wybierz karte ktora chcesz zagrac')
         if sylaba:
-            wybrana_karta = self.gracze[self.turn].wyloz_karte(sylaba) # id albo pelna wartosc zwraca(return) wartosc karty np 'ba'
+            wybrana_karta = self.gracze[self.turn].wyloz_karte(
+                sylaba)  # id albo pelna wartosc zwraca(return) wartosc karty np 'ba'
             if self.sprawdz_poprawnosc_slowa(wybrana_karta):
                 self.wartownik = wybrana_karta
                 self.gracze[self.turn].odrzuc_karte(wybrana_karta)
@@ -68,18 +77,21 @@ class Game:
         for _ in range(2):
             temp_lista = []
             for __ in range(6):
+                print(temp_lista)
                 temp_lista.append(self.karty[-1])
                 self.karty.pop()
             gracz = Player(temp_lista)
             self.gracze.append(gracz)
 
-    def koniec_gry(self):
+    def czy_zakonczyc_gre(self):
         if len(self.gracze[self.turn].karty_gracza) == 0:
-           return True
+            self.koniec_gry = self.turn
+            return True
 
 
 class Player:
     id = 0
+
     # def __init__(self, id):
     #     self.id = id
 
@@ -98,8 +110,5 @@ class Player:
     def pobierz_karte(self, karta):
         self.karty_gracza.append(karta)
 
-
-
-
-gra = Game()
-gra.start_gry()
+# gra = Game()
+# gra.start_gry()
